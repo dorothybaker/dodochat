84ba89b0a522512276b41f6a9b6ec1a4397af665
Dorothy Baker <tbakerdorothy@gmail.com>
2024-04-14 19:40:33 +0300

    building

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { config } from "dotenv";
+import path from "path";
 
 import authRouter from "./routes/auth.route.js";
 import { connectToDB } from "./utils/connectToDB.js";
@@ -12,17 +13,21 @@ import { app, server } from "./socket/socket.js";
 config();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
-app.get("/", (req, res) => {
-  res.send("This is the main route!");
-});
-
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, (req, res) => {
   connectToDB();
